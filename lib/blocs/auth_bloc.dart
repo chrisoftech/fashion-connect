d0a34f871dc1ee18e0a247d897864c6b9ed0c7b7
cjs10lib <34670425+cjs10lib@users.noreply.github.com>
2019-03-05 16:23:17 +0000

    Created auth_state, auth_event & auth_bloc Implemented app_started, logged_in & logged_out events in authBloc

---
+++ lib/blocs/auth_bloc.dart
@@ -0,0 +1,108 @@
+import 'package:equatable/equatable.dart';
+import 'package:fashion_connect/models/models.dart';
+import 'package:fashion_connect/repositories/repositories.dart';
+import 'package:meta/meta.dart';
+import 'package:bloc/bloc.dart';
+
+// AUTHETICATION STATES
+abstract class AuthState extends Equatable {}
+
+class AuthUnInitialized extends AuthState {
+  @override
+  String toString() => 'AuthUnInitialized';
+}
+
+class AuthAuthenticated extends AuthState {
+  @override
+  String toString() => 'AuthAuthenticated';
+}
+
+class AuthUnauthenticated extends AuthState {
+  @override
+  String toString() => 'AuthUnauthenticated';
+}
+
+class AuthLoading extends AuthState {
+  @override
+  String toString() => 'AuthLoading';
+}
+
+// AUTHENTICATION EVENTS
+abstract class AuthEvent extends Equatable {
+  AuthEvent([List props = const []]) : super(props);
+}
+
+class AppStarted extends AuthEvent {
+  @override
+  String toString() => 'AppStarted';
+}
+
+class LoggedIn extends AuthEvent {
+  final User user;
+
+  LoggedIn({@required this.user}) : super([user]);
+
+  @override
+  String toString() => '{ user: ${user.toString()} }';
+}
+
+class LoggedOut extends AuthEvent {
+  @override
+  String toString() => 'LoggedOut';
+}
+
+// AUTHENTICATION BLOC
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final AuthRepository authRepository;
+
+  AuthBloc({@required this.authRepository}) : assert(authRepository != null);
+
+  @override
+  AuthState get initialState => AuthUnInitialized();
+
+  void onAppStart() {
+    dispatch(AppStarted());
+  }
+
+  void onLoggedIn({@required User user}) {
+    dispatch(LoggedIn(user: user));
+  }
+
+  void onLoggedOut() {
+    dispatch(LoggedOut());
+  }
+
+  @override
+  Stream<AuthState> mapEventToState(
+      AuthState currentState, AuthEvent event) async* {
+    if (event is AppStarted) {
+      final bool isAuthenticated = await authRepository.isAuthenticated();
+
+      if (isAuthenticated) {
+        yield AuthAuthenticated();
+      } else {
+        yield AuthUnauthenticated();
+      }
+    }
+
+    if (event is LoggedIn) {
+      yield AuthLoading();
+
+      final _user = User(
+          uid: '123456',
+          username: '0270747772',
+          imageUrl: 'imageUrl',
+          userRight: 'admin');
+
+      await authRepository.persistUser(user: _user);
+      yield AuthAuthenticated();
+    }
+
+    if (event is LoggedOut) {
+      yield AuthLoading();
+
+      await authRepository.signout();
+      yield AuthUnauthenticated();
+    }
+  }
+}
