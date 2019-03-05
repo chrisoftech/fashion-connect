77624354a51b59463680fd971038082124f1f7ca
cjs10lib <34670425+cjs10lib@users.noreply.github.com>
2019-03-05 17:43:44 +0000

    Created loading_indicator widget Renamed splash_page to splash_screen Export loading_indicator & splash_screen in utilities.dart

--- lib/utilities/splash_screen.dart
+++ lib/utilities/splash_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Text('Splash Screen'),
       ),
     );
   }
